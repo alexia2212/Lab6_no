94ee758bed50bcf28c95f613c68bb21b0b419e7a
alexia2212 <alexia_jg@outlook.es>
2023-05-23 20:29:15 -0500

    first commit

---
+++ src/main/java/com/example/laboratorio6/Laboratorio6Application.java
@@ -0,0 +1,13 @@
+package com.example.laboratorio6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laboratorio6Application.class, args);
+    }
+
+}
